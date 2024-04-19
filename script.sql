ee8823101fda0c29a03ecfe784222a496e06aeb6
Mayson Kotlyar Maksim <maksimmyson33@gmail.com>
2024-04-19 19:48:39 +0300

    Create Table

---
+++ script.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE [db_sql_16_04]
+GO
+USE [db_sql_16_04]
+
+CREATE TABLE Examination (
+    Id INT PRIMARY KEY IDENTITY (1,1),
+    Name NVARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE Doctors (
+    Id INT PRIMARY KEY IDENTITY,
+    Name NVARCHAR(MAX) NOT NULL,
+    Premium MONEY NOT NULL DEFAULT 0 CHECK (Premium >= 0),
+    Salary MONEY NOT NULL CHECK (Salary > 0),
+    Surname NVARCHAR(MAX) NOT NULL
+);
+
+CREATE TABLE Wards (
+    Id INT PRIMARY KEY IDENTITY,
+    Name NVARCHAR(20) NOT NULL UNIQUE,
+    Places INT NOT NULL CHECK (Places >= 1),
+    DepartmentId INT NOT NULL
+);
+
+CREATE TABLE DoctorsExaminations (
+    Id INT PRIMARY KEY IDENTITY (1,1),
+    EndTime TIME NOT NULL,
+    StartTime TIME NOT NULL CHECK (StartTime >= '08:00' AND StartTime <= '18:00'),
+    DoctorId INT NOT NULL,
+    ExaminationId INT NOT NULL,
+    WardId INT NOT NULL
+);
+
+CREATE TABLE Departments (
+    Id INT PRIMARY KEY IDENTITY (1,1),
+    Building INT NOT NULL CHECK (Building BETWEEN 1 AND 5),
+    Name NVARCHAR(100) NOT NULL UNIQUE
+);
